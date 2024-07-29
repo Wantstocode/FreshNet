7473cc12126748e3b47f1c59be1b83f477f3fce2
kirancb6620 <120893466+Wantstocode@users.noreply.github.com>
2024-07-29 23:17:40 +0530

    Delete src directory

--- src/main/java/com/sheryians/major/repository/RolesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sheryians.major.repository;
-
-import com.sheryians.major.model.Roles;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolesRepository extends JpaRepository<Roles,Integer> {
-}
